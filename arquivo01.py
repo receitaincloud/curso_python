65705a4ae05c90c84d81e7b56581add7960b6fe1
receitaincloud <91987982+receitaincloud@users.noreply.github.com>
2022-01-15 11:56:38 -0300

    primeiro arquivo

---
+++ arquivo01.py
@@ -0,0 +1,34 @@
+# Declaração de variáveis
+var_texto_1 = "COD-Esse-COD "
+var_texto_2 = "quebra "
+var_texto_3 = "cabeca "
+var_texto_4 = "ta "
+var_texto_5 = "muito "
+var_texto_6 = "estranho!"
+var_int = 100
+
+# Interação do usuario inserindo valores
+#interacao_um = input("Digite o valor do primeiro numero: ")
+#interacao_dois = input("Digite o valor do segundo numero: ")
+
+# Somando variaveis
+#resultado_soma = int(interacao_um) + int(interacao_dois)
+#print(resultado_soma)
+
+# Segmentandp texto de variável
+print(var_texto_1[4:-5], var_texto_2, var_int)
+
+# Recomendação das Classes utilizando ponto
+print(var_texto_1.lower())
+
+# Tipos extras de variáveis: Lista
+
+notas_aluno = [10, 8, 8, 9]
+print(notas_aluno[3])
+
+
+
+
+
+
+
